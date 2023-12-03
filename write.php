aa965f237a914f2a245c7bb09b5a0210f6dcac0e
Totomomokazu <kanketomokazu@gmail.com>
2023-12-03 21:14:45 +0900

    微修正

--- write.php
+++ write.php
@@ -7,14 +7,18 @@
 </head>
 <body>
     <?php
+        // XSS：クロスサイトスクリプティングの関数を追加
+        function h($val){
+            return htmlspecialchars($val, ENT_QUOTES);
+        }
         $name=$_POST["name"];
         $birthplace=$_POST["birthplace"];
         $time=date("Y-m-d H:i:s");
         file_put_contents('data/data.txt', $time . $name . $birthplace, FILE_APPEND);
     ?>
-
-
     <p>提出しました</p>
+    <p><?= h($name);  ?></p>
+    <p><?= h($birthplace);  ?></p>
     <ul>
         <!-- hrefに結果を表示させるファイルに遷移する -->
         <li><a href="read.php">アンケート結果を確認する</a></li>
